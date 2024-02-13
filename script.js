fb3a969d0b9f877d2e6dc0b7290e0404494df6c8
Sujan P <84078948+sujan-026@users.noreply.github.com>
2024-02-13 20:41:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('proposal-btn').addEventListener('click', function() {
+    alert('I love you! Happy Valentine\'s Day, my dear!');
+});
+
+document.getElementById('heart').addEventListener('click', function() {
+    document.getElementById('hidden-message').style.display = 'block';
+});
